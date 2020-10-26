7b6ce77a0e6fcbf5546441ed2099d0782c33594d
Megan Davidson <megan.davidson@4cplatform.co.uk>
2020-10-26 21:48:12 +0000

    Fix wrong route name in Auth Middleware

--- app/Http/Middleware/Authenticate.php
+++ app/Http/Middleware/Authenticate.php
@@ -23,7 +23,7 @@ class Authenticate extends Middleware
     protected function redirectTo($request): ?string
     {
         if (!$request->expectsJson()) {
-            return route('login');
+            return route('user.login');
         }
 
         return null;
