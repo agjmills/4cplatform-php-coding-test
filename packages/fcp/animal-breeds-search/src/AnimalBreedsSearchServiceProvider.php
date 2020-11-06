172ebeaa0dc13bb47659210327d66bc549502802
Alex Mills <alex@asdfx.us>
2020-11-06 12:55:13 +0000

    register command for searching breeds

--- packages/fcp/animal-breeds-search/src/AnimalBreedsSearchServiceProvider.php
+++ packages/fcp/animal-breeds-search/src/AnimalBreedsSearchServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace Fcp\AnimalBreedsSearch;
 
+use Fcp\AnimalBreedsSearch\Commands\SearchBreeds;
 use Illuminate\Support\ServiceProvider;
 
 /**
@@ -11,24 +12,13 @@ use Illuminate\Support\ServiceProvider;
  */
 class AnimalBreedsSearchServiceProvider extends ServiceProvider
 {
-    /**
-     * Perform post-registration booting of services.
-     *
-     * @return void
-     */
     public function boot(): void
     {
-        // Publishing is only necessary when using the CLI.
         if ($this->app->runningInConsole()) {
             $this->bootForConsole();
         }
     }
 
-    /**
-     * Register any package services.
-     *
-     * @return void
-     */
     public function register(): void
     {
         $this->mergeConfigFrom(__DIR__ . '/../config/animal-breeds-search.php', 'animal-breeds-search');
@@ -38,18 +28,8 @@ class AnimalBreedsSearchServiceProvider extends ServiceProvider
         });
     }
 
-    /**
-     * Console-specific booting.
-     *
-     * @return void
-     */
     protected function bootForConsole(): void
     {
-        // Publish the translation files.
-
-        // Publish the configuration file.
-
-        // Registering package commands.
-        $this->commands([]);
+        $this->commands([SearchBreeds::class]);
     }
 }
